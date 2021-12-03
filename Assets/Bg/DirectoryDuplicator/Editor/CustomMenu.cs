919dc967f625bec3c6793660d0ea87cf8ae468f5
k-okawa <kyohei.okawa@10antz.co.jp>
2021-12-03 19:08:00 +0900

    Test YamlDotNet

--- Assets/Bg/DirectoryDuplicator/Editor/CustomMenu.cs
+++ Assets/Bg/DirectoryDuplicator/Editor/CustomMenu.cs
@@ -1,5 +1,7 @@
 using System.IO;
 using System.Linq;
+using Bg.DirectoryDuplicator.YamlDotNet.Core.Tokens;
+using Bg.DirectoryDuplicator.YamlDotNet.RepresentationModel;
 using UnityEditor;
 using UnityEngine;
 
@@ -22,6 +24,32 @@ namespace Bg.DirectoryDuplicator.Editor {
                 .Where(itr => Path.GetExtension(itr) != ".meta").ToList();
             
             
+        }
+
+        [MenuItem("Assets/Bg/TestYaml")]
+        static void TestYaml() {
+            Object[] selectedAsset = Selection.GetFiltered(typeof(Object), SelectionMode.Assets);
+            string path = AssetDatabase.GetAssetPath(selectedAsset.First());
+            var input = new StringReader(File.ReadAllText(path));
+            var yaml = new YamlStream();
+            yaml.Load(input);
+
+            foreach (var doc in yaml.Documents) {
+                var mapping = (YamlMappingNode)doc.RootNode;
+                foreach (var entry in mapping.Children)
+                {
+                    Debug.Log($"{((YamlScalarNode)entry.Key).Value}");
+                    var childMap = (YamlMappingNode)entry.Value;
+                    foreach (var childEntry in childMap.Children) {
+                        if (childEntry.Value.NodeType == YamlNodeType.Scalar) {
+                            Debug.Log($"{((YamlScalarNode)childEntry.Key).Value}:{((YamlScalarNode)childEntry.Value).Value}");
+                        } else if(childEntry.Value.NodeType == YamlNodeType.Mapping) {
+                            Debug.Log(childEntry.Value.ToString());
+                        }
+                    }
+                }
+                
+            }
         }
     }
 }
